b903186ad5484772b70a31756fcd2df1e2c383ff
Utkarsh <utkarshadlakha1702@gmail.com>
2025-05-01 01:00:28 +0530

    Setting up basic layout

--- GsmEngine/GsmEngine/GsmEngine.cpp
+++ GsmEngine/GsmEngine/GsmEngine.cpp
@@ -2,19 +2,40 @@
 //
 
 #include <iostream>
+using namespace std;
+#include <Windows.h>
+
+//Setting a default height and width of game
+int nScreenHeight = 40;
+int nScreenWidth = 120;
+
+//Setting player X and Y
+float playerX = 0.0;
+float playerY = 0.0;
+//PLayer angle
+float playerA = 0.0;
+
+//Mini Map (#-  Walls   .-  Empty space)
+int nMapWidth = 16;
+int nMapHeight = 16;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Setting up variables
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+
+	DWORD dwBytesWritten = 0;
+
+	//Game loop
+	while (1)
+	{
+		//Setting array and showing character on screen
+	screen[nScreenWidth * nScreenHeight - 1] = '\0';
+	//{0,0} means start writing from top-left
+	WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
+    
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
